dbd8e8d997974de6911e5ec0722bb3d5656c09f0
abdellah <abdellahedaoudi80@gmail.com>
2024-07-26 23:19:09 +0100

    SERVER URL

--- app/Context/MyContext.js
+++ app/Context/MyContext.js
@@ -7,9 +7,9 @@ export const MyProvider = ({ children }) => {
   const [userDetails, setUserDetails] = useState([]);
   // const CLIENT_URL = "http://localhost:3000";
   // const SERVER_URL = "http://localhost:9999";
+  // const SERVER_URL = "https://saasapp-api.onrender.com";
   const CLIENT_URL = "https://chatfloww.vercel.app";
   const SERVER_URL = "https://saas-app-api.vercel.app";
-  // const SERVER_URL = "https://saasapp-api.onrender.com";
 
    // Get Users
    useEffect(() => {
