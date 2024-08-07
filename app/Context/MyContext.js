1818f5edbd465ca70a3af3680f49b2896f4ea9d6
abdellah <abdellahedaoudi80@gmail.com>
2024-08-07 18:43:57 +0100

    change name domain  to CLIENT_URL

--- app/Context/MyContext.js
+++ app/Context/MyContext.js
@@ -17,7 +17,7 @@ export const MyProvider = ({ children }) => {
   // const CLIENT_URL = "http://localhost:3000";
   // const SERVER_URL = "http://localhost:9999" ;
   // const SERVER_URL_V = "http://localhost:9999" ;
-   const CLIENT_URL = "https://chatfloww.vercel.app";
+   const CLIENT_URL = "https://linkerfolio.vercel.app";
    const SERVER_URL = "https://socketserver-muhp.onrender.com";
    const SERVER_URL_V = "https://saas-app-api.vercel.app";
 
