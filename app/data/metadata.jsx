402ec877f4e4de7486587e99789b99a32415ae3b
abdellah <abdellahedaoudi80@gmail.com>
2024-10-07 19:15:06 +0100

    Improve MetaData

--- app/data/metadata.jsx
+++ app/data/metadata.jsx
@@ -1,11 +1,57 @@
-
-
 export const Metadata = {
-    title: "LinkerFolio",
-    description: "LinkerFolio is an innovative platform for managing your portfolio and social media links seamlessly. Create, customize, and share your personal or professional links with ease.",
-    keywords: "LinkerFolio, portfolio management, social media links, personal branding, professional portfolio, link manager, online portfolio, social media integration, custom profiles",
-    author: "Abdellah Edaoudi",
-    robots: "index, follow",
-    viewport: "width=device-width, initial-scale=1.0",
-  };
-  
+  title: "LinkerFolio - Your Ultimate Global Digital Portfolio & Social Hub",
+  description: "Discover LinkerFolio, your go-to platform for managing a professional digital portfolio and social media links. Showcase your personal brand globally with our customizable profiles, automatic translation, and unique QR codes.",
+  keywords: "LinkerFolio, digital portfolio, online presence, personal branding, social media integration, customizable profiles, QR code generator, global networking, career opportunities, job search, resume showcase",
+  author: "Abdellah Edaoudi",
+  robots: "index, follow",
+  viewport: "width=device-width, initial-scale=1.0",
+  openGraph: {
+      title: "LinkerFolio - Your Ultimate Global Digital Portfolio & Social Hub",
+      description: "Elevate your professional identity with LinkerFolio. Manage your portfolio and social media links, generate QR codes, and share seamlessly with a global audience.",
+      image: "https://res.cloudinary.com/dssrnghtr/image/upload/v1726176794/mptxnsr28gdxrdzpgp5i.png",
+      url: "https://linkerfolio.vercel.app",
+      type: "website",
+      locale: "en_US",
+      site_name: "LinkerFolio",
+      profile: {
+          first_name: "Abdellah",
+          last_name: "Edaoudi",
+          username: "LinkerFolio",
+          gender: "male",
+      },
+  },
+  twitter: {
+      card: "summary_large_image",
+      title: "LinkerFolio - Your Ultimate Global Digital Portfolio & Social Hub",
+      description: "Explore LinkerFolio, the platform that transforms how you showcase your professional identity. Create and share your digital portfolio and social media links globally.",
+      image: "https://res.cloudinary.com/dssrnghtr/image/upload/v1728324724/hykjr2ggahvrg1ylyf75.png",
+      creator: "@Edaoudi_abde",
+      site: "@LinkerFolio",
+  },
+  additionalMetaTags: [
+      {
+          property: "theme-color",
+          content: "#ffffff",
+      },
+      {
+          name: "robots",
+          content: "index, follow",
+      },
+      {
+          name: "google-site-verification",
+          content: "google-site-verification=AKe-FSZeT2tt0eIiD4oTfur9DrT_1WgN9vhJXjnD6Go",
+      },
+      {
+          name: "application-name",
+          content: "LinkerFolio",
+      },
+      {
+          name: "twitter:site",
+          content: "@LinkerFolio",
+      },
+      {
+          name: "twitter:creator",
+          content: "@Edaoudi_abde",
+      },
+  ],
+};
